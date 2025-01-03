7e5545f4b689db829b9b347234a8fa3c8614de02
二十年没去过星巴克 <107996273+6666-H@users.noreply.github.com>
2025-01-03 12:19:06 +0800

    Update ad_rewrite.py

--- sync/ad_rewrite.py
+++ sync/ad_rewrite.py
@@ -12,17 +12,10 @@ README_PATH = "README-rewrite.md"
 
 # 规则源列表
 REWRITE_SOURCES = {
-    "surge去广告": "https://raw.githubusercontent.com/QingRex/LoonKissSurge/refs/heads/main/Surge/Official/%E6%96%B0%E6%89%8B%E5%8F%8B%E5%A5%BD%E3%81%AE%E5%8E%BB%E5%B9%BF%E5%91%8A%E9%9B%86%E5%90%88.official.sgmodule",
+ 
     "whatshubs开屏屏蔽": "https://whatshub.top/rewrite/adultraplus.conf",
     "whatshub微信屏蔽": "https://whatshub.top/rewrite/wechatad.conf",
-    "whatshub油管优化": "https://whatshub.top/rewrite/youtube.conf",
-    "chxm去广告": "https://raw.githubusercontent.com/chxm1023/Advertising/main/AppAd.conf",
-    "可莉微信屏蔽": "https://kelee.one/Tool/Loon/Plugin/WexinMiniPrograms_Remove_ads.plugin",
-    "墨鱼微信广告": "https://raw.githubusercontent.com/ddgksf2013/Rewrite/master/AdBlock/Applet.conf",
-    "墨鱼去开屏V2.0": "https://raw.githubusercontent.com/ddgksf2013/Rewrite/master/AdBlock/StartUp.conf",
-    "广告拦截精简版": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rewrite/QuantumultX/AdvertisingLite/AdvertisingLite.conf",
-    "去广告重写": "https://raw.githubusercontent.com/fmz200/wool_scripts/main/QuantumultX/rewrite/chongxie.txt",
-    "整合广告拦截": "https://raw.githubusercontent.com/DivineEngine/Profiles/master/Quantumult/Rewrite/Block/Advertising.conf"
+    "whatshub油管优化": "https://whatshub.top/rewrite/youtube.conf"
 }
 
 def setup_directory():
