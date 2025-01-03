e017dee6838149dac6361422786fd585733ab6ca
二十年没去过星巴克 <107996273+6666-H@users.noreply.github.com>
2025-01-03 12:28:20 +0800

    Update ad_rule.py

--- sync/ad_rule.py
+++ sync/ad_rule.py
@@ -12,17 +12,7 @@ README_PATH = "README-rule.md"
 
 # 分流规则源列表
 FILTER_SOURCES = {
-    "AntiAD": "https://whatshub.top/rule/AntiAD.list",
-    "Ads_ml": "https://github.com/thNylHx/Tools/raw/main/Ruleset/Surge/Block/Ads_ml.list",
-    "Reject_Rule": "https://raw.githubusercontent.com/Code-Dramatist/Rule_Actions/main/Reject_Rule/Reject_Rule.rule",
-    "AdBlock": "https://raw.githubusercontent.com/zqzess/rule_for_quantumultX/refs/heads/master/QuantumultX/rules/AdBlock.list",
-    "AdvertisingLite": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rewrite/QuantumultX/AdvertisingLite/AdvertisingLite.list",
-    "Reject": "https://raw.githubusercontent.com/Irrucky/Tool/main/Surge/rules/Reject.list",
-    "SKK_Reject": "https://ruleset.skk.moe/List/non_ip/reject.conf",
-    "Dler_AdBlock": "https://raw.githubusercontent.com/dler-io/Rules/refs/heads/main/Surge/Surge%203/Provider/AdBlock.list",
-    "SKK_IP_Reject": "https://ruleset.skk.moe/List/ip/reject.conf",
-    "NobyDa_AdRule": "https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/AdRule.list",
-    "Adrules": "https://adrules.top/adrules.list"
+    "AntiAD": "https://whatshub.top/rule/AntiAD.list"
 }
 
 def setup_directory():
