964660a2dfd3931a9786cb536de8eaebdf13d38e
二十年没去过星巴克 <107996273+6666-H@users.noreply.github.com>
2025-01-03 09:04:22 +0800

    Update ad_rewrite_sync.py

--- rules/sync/ad_rewrite_sync.py
+++ rules/sync/ad_rewrite_sync.py
@@ -12,7 +12,8 @@ README_PATH = "README.md"
 
 # 规则源列表
 REWRITE_SOURCES = {
-    "微信广告": "https://raw.githubusercontent.com/ddgksf2013/Rewrite/master/AdBlock/Weixin.conf",
+    "墨鱼微信广告": "https://raw.githubusercontent.com/ddgksf2013/Rewrite/master/AdBlock/Applet.conf",
+    "墨鱼去开屏V2.0": "https://raw.githubusercontent.com/ddgksf2013/Rewrite/master/AdBlock/StartUp.conf",
     "广告拦截精简版": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rewrite/QuantumultX/AdvertisingLite/AdvertisingLite.conf",
     "去广告重写": "https://raw.githubusercontent.com/fmz200/wool_scripts/main/QuantumultX/rewrite/chongxie.txt",
     "整合广告拦截": "https://raw.githubusercontent.com/DivineEngine/Profiles/master/Quantumult/Rewrite/Block/Advertising.conf"
