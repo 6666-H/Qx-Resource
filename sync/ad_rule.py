a0f525ab9d196944be412cf5af4ed5b39b235ec0
二十年没去过星巴克 <107996273+6666-H@users.noreply.github.com>
2025-01-21 09:48:39 +0800

    Update ad_rule.py

--- sync/ad_rule.py
+++ sync/ad_rule.py
@@ -16,17 +16,14 @@ WHITE_LIST_URL = "https://raw.githubusercontent.com/6666-H/QuantumultX-Resource/
 # 分流规则源列表
 FILTER_SOURCES = {
     "AntiAD":"https://raw.githubusercontent.com/deezertidal/QuantumultX-Rewrite/refs/heads/master/rule/AntiAD.list",
+    "Advertising": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Advertising/Advertising.list",
     "Reect_yaml":"https://raw.githubusercontent.com/6666-H/QuantumultX-Resource/refs/heads/main/manual/rule/Reject.yaml",
-    "AD_ALL": "https://raw.githubusercontent.com/6666-H/QuantumultX-Resource/refs/heads/main/manual/rule/Ad_All.list",
     "Ads_ml": "https://github.com/thNylHx/Tools/raw/main/Ruleset/Surge/Block/Ads_ml.list",
     "Reject_Rule": "https://raw.githubusercontent.com/Code-Dramatist/Rule_Actions/main/Reject_Rule/Reject_Rule.rule",
-    "AdBlock": "https://raw.githubusercontent.com/zqzess/rule_for_quantumultX/refs/heads/master/QuantumultX/rules/AdBlock.list",
-    "AdvertisingLite": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rewrite/QuantumultX/AdvertisingLite/AdvertisingLite.list",
     "Reject": "https://raw.githubusercontent.com/Irrucky/Tool/main/Surge/rules/Reject.list",
     "SKK_Reject": "https://ruleset.skk.moe/List/non_ip/reject.conf",
     "Dler_AdBlock": "https://raw.githubusercontent.com/dler-io/Rules/refs/heads/main/Surge/Surge%203/Provider/AdBlock.list",
-    "SKK_IP_Reject": "https://ruleset.skk.moe/List/ip/reject.conf",
-    "NobyDa_AdRule": "https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/AdRule.list"
+    "SKK_IP_Reject": "https://ruleset.skk.moe/List/ip/reject.conf"
 }
 
 def get_beijing_time():
