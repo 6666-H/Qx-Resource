27416ce0492ae21a71067c2809e9a447e70a3abc
二十年没去过星巴克 <107996273+6666-H@users.noreply.github.com>
2025-02-15 17:42:43 +0800

    Update Rule_Ad_Lite.py

--- Sync/Rule_Ad_Lite.py
+++ Sync/Rule_Ad_Lite.py
@@ -15,7 +15,6 @@ WHITE_LIST_URL = "https://raw.githubusercontent.com/6666-H/QuantumultX-Resource/
 
 # 分流规则源列表
 FILTER_SOURCES = {
-    "Manual_AD":"https://raw.githubusercontent.com/6666-H/QuantumultX-Resource/refs/heads/main/Manual/Rule/Ad_All.list",
     "AntiAD":"https://raw.githubusercontent.com/deezertidal/shadowrocket-rules/refs/heads/main/rule/ADLite.list",
     "AdvertisingLite": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rewrite/QuantumultX/AdvertisingLite/AdvertisingLite.list"
 }
