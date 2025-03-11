43118789208b8991715fabb820d2085fbbfbb957
二十年没去过星巴克 <107996273+6666-H@users.noreply.github.com>
2025-03-11 17:04:21 +0800

    Update Rewrite_Tool.py

--- Sync/Rewrite_Tool.py
+++ Sync/Rewrite_Tool.py
@@ -18,7 +18,11 @@ class Config:
             "京东比价": "https://raw.githubusercontent.com/mw418/Loon/main/script/jd_price.js",
             "1998解锁": "https://raw.githubusercontent.com/Yu9191/Rewrite/main/1998.js",
             "siri接入4o":"https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/QuantumultX/Siri/aireply/aireply.js",
-            "叮当猫合集":"https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js"
+            "叮当猫合集":"https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js",
+            "body合集":"https://raw.githubusercontent.com/Yu9191/Rewrite/refs/heads/main/Revenuecat.js",
+            "墨鱼解锁合集":"https://raw.githubusercontent.com/ddgksf2013/Modules/refs/heads/main/RevenueCat.Vip.sgmodule",
+            "sooyaaabo解锁合集":"https://raw.githubusercontent.com/sooyaaabo/Loon/main/Plugin/RevenueCat.plugin",
+            "枫叶脚本解锁":"https://raw.githubusercontent.com/afengye/QX/main/crack.js"
         }
 
 class RuleProcessor:
