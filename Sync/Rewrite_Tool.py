f2386bde6c418a197683a7feaaae72c5e72ed332
二十年没去过星巴克 <107996273+6666-H@users.noreply.github.com>
2025-08-25 18:31:59 +0800

    Update Rewrite_Tool.py

--- Sync/Rewrite_Tool.py
+++ Sync/Rewrite_Tool.py
@@ -15,7 +15,7 @@ class Config:
         # 规则源
         self.REWRITE_SOURCES = {
             "谷歌重定向": "https://raw.githubusercontent.com/6666-H/Qx-Resource/refs/heads/main/Manual/Rewrite/GoogleToSearch.config",
-             "ReLens": "https://raw.githubusercontent.com/6666-H/Qx-Resource/refs/heads/main/Manual/Rewrite/ReLens.config",
+            "ReLens": "https://raw.githubusercontent.com/6666-H/Qx-Resource/refs/heads/main/Manual/Rewrite/ReLens.config",
             "京东比价": "https://raw.githubusercontent.com/githubdulong/Script/master/Surge/jd_price.sgmodule"
         }
 
