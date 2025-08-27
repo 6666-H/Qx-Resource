180f666955c0801c8a12cb5930df19bc573a8ed0
二十年没去过星巴克 <107996273+6666-H@users.noreply.github.com>
2025-08-27 16:17:29 +0800

    Update Rewrite_Tool.py

--- Sync/Rewrite_Tool.py
+++ Sync/Rewrite_Tool.py
@@ -15,7 +15,6 @@ class Config:
         # 规则源
         self.REWRITE_SOURCES = {
             "谷歌重定向":"https://raw.githubusercontent.com/6666-H/Qx-Resource/refs/heads/main/Manual/Rewrite/GoogleToSearch.config",
-            "微信110解锁被屏蔽的URL":"https://raw.githubusercontent.com/ddgksf2013/Rewrite/master/Function/UnblockURLinWeChat.conf",
             "京东比价": "https://raw.githubusercontent.com/githubdulong/Script/master/Surge/jd_price.sgmodule",
             "ReLens":"https://raw.githubusercontent.com/chxm1023/Rewrite/main/ReLens.js",
             "懒人听书": "https://raw.githubusercontent.com/WeiGiegie/666/main/lrts.js"
